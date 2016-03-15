46e8e15d0ce9f7634f15550f51da773ba78f16fd
Hrishikesh VK <kv_hrishikesh@yahoo.com>
2016-03-16 00:46:40 +0530

    added more tests with large numbers

--- gcd.h
+++ gcd.h
@@ -1,9 +1,18 @@
+#include <limits.h>
+#include <stdlib.h>
 
 template <typename Int_Type>
-Int_Type gcd(Int_Type i, Int_Type j){
+Int_Type i_gcd(Int_Type i, Int_Type j){
 
 	if(i == 0)
 		return j;
-	return gcd(j % i, i);
+	return i_gcd(j % i, i);
+}
+
+template <typename Int_Type>
+Int_Type gcd(Int_Type i, Int_Type j){
+	Int_Type g = i_gcd<Int_Type>(i, j);
+
+	return abs(g); //// Might do abs() on an INT_MIN ... TODO
 }
 
--- unit_tests/simple_tests.cpp
+++ unit_tests/simple_tests.cpp
@@ -37,5 +37,38 @@ BOOST_AUTO_TEST_CASE( simple30_45 ){
 	std::cout << " ... done\n";
 }
 
+BOOST_AUTO_TEST_CASE( extreme_1 ){
+	std::cout << "Running Test # " << test_number++ << "\n";
+	BOOST_REQUIRE( gcd(0x7fffffff, 1) == 1);
+	std::cout << " ... done\n";
+}
+
+BOOST_AUTO_TEST_CASE( extreme_top_2 ){
+	std::cout << "Running Test # " << test_number++ << "\n";
+	BOOST_REQUIRE( gcd(0x7fffffff, 0x7fffffff) == 0x7fffffff);
+	BOOST_REQUIRE( gcd(0x7fffffff, 0x7ffffffe) == 1);
+	std::cout << " ... done\n";
+}
+
+BOOST_AUTO_TEST_CASE( extreme_neg_2 ){
+	std::cout << "Running Test # " << test_number++ << "\n";
+	BOOST_REQUIRE( gcd(-0x7fffffff, 0x7ffffffe) == 1);
+	BOOST_REQUIRE( gcd(0x7fffffff, -0x7ffffffe) == 1);
+	BOOST_REQUIRE( gcd(-0x7fffffff, -0x7ffffffe) == 1);
+
+	BOOST_REQUIRE( gcd( (0x1 << 31), 0x7ffffffe) == 2);
+
+	BOOST_REQUIRE( gcd( (0x1 << 31), 2*2*2*2*3*5) == 2*2*2*2);
+
+	std::cout << " ... done\n";
+}
+
+BOOST_AUTO_TEST_CASE( extreme_2_neg_min ){
+	std::cout << "Running Test # " << test_number++ << "\n";
+	int ans = gcd<int>(0x1 << 31, 0x1 << 31);
+	BOOST_REQUIRE(ans == 0x1 << 31); //// Using abs on an INT_MIN. TODO rectification.
+	std::cout << " ... done\n";
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
